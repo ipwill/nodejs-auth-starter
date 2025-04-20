0afc9b92725d318b4cecd7ba6d777b6638ec10de
graydee <142386274+httpcg@users.noreply.github.com>
2025-04-20 01:13:32 +0000

    Update webpack.config.cjs

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: ['./src/app.js', './public/js/script.js'],
+  entry: ['./src/app.js'],
   output: {
     path: path.resolve(__dirname, 'public/js'),
     filename: 'bundle.js',
